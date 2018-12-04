7a782b860d7d5833195b2b3baf792f6a64fe928c
dmn <dborowiecki7@gmail.com>
2018-12-04 15:03:43 +0100

    Added distribution interface

---
+++ Zad4/src/main/java/com/example/shdemo/service/DistributionManagerI.java
@@ -0,0 +1,23 @@
+package com.example.shdemo.service;
+
+import com.example.shdemo.domain.Alcohol;
+import com.example.shdemo.domain.Person;
+import com.example.shdemo.domain.Producer;
+
+import java.util.List;
+
+public interface DistributionManagerI {
+	
+	void addProducer(Producer producer);
+	List<Producer> getAllProducers();
+	void deleteProducer(Producer producer);
+	Producer findProducerByCode(String code);
+	
+	Long addNewAlcohol(Alcohol alcohol);
+	List<Alcohol> getAvailableAlcohols();
+	Alcohol findAlcoholById(Long id);
+
+	List<Alcohol> getProducersAlcohols(Producer producer);
+	void removeProducerAlcohols(Long producerId, Long alcoholId);
+
+}
